9adcc496feb169c5e56b851c1398604cfd484c6f
jremmons <emmons.john@gmail.com>
2017-08-01 10:45:14 -0700

    added simple hw example

---
+++ src/capture/hw.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    // create a buffer with the checkboard output
+
+    // encode it into h264 format (output as .mp4 file) 60 frames
+    
+    std::cout << "hw\n";
+    return 0;
+}
