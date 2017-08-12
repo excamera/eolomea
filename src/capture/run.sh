c178c11552ab791c7f4d3c034e8201f53d40f61e
jremmons <emmons.john@gmail.com>
2017-08-12 14:02:46 -0700

    reorganized the capture dir

--- src/capture/run.sh
+++ src/capture/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./capture -d 0 -m 14 -p 3 -v video.raw -l /dev/null -D 1 -b 2000 -f 2 -q 32 -B beforeFile.txt -A afterFile.txt > error.txt
+./ps4_degrader -d 0 -m 14 -p 3 -v video.raw -l /dev/null -D 1 -b 2000 -f 2 -q 32 -B beforeFile.txt -A afterFile.txt > error.txt
